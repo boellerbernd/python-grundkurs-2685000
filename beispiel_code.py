98a09f488f393767967190ff3573feab139b29c3
boellerbernd <boellerbernd1000@gmail.com>
2024-12-04 13:32:43 +0000

    added beispiel_code.py

---
+++ beispiel_code.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+a = 10
+b = 5
+c = 3
+
+# Addition
+summe = a + b
+print("Summe:",summe)
+
+# Subtraktion
+differenz = a - b
+print("Differenz:",differenz)
+
+# Multiplikation
+produkt = a * b
+print("Produkt:",produkt)
+
+# Division
+quotient = a / b
+print("Quotient:",quotient)
+
+# ganzzahlige Division
+ganzzahl_quotient = a // 3
+print("Ganzzahliger Quotient:",ganzzahl_quotient)
+
+# Modulo
+rest = a % 3
+print("Rest:",rest)
+
+# Potenzierung
+potenz = 2 ** c
+print("2 hoch 3:", potenz)
+
+aa = 8
+bb = 2
+
+print(aa + bb)
+print(aa - bb)
+print(aa * bb)
+print(aa / bb)
