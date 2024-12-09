431140058717508a01b1e81ea2df40b2288b67fb
boellerbernd <boellerbernd1000@gmail.com>
2024-12-09 15:32:28 +0000

    added beispiel_code.py

---
+++ beispiel_code.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Beispiel für argparse")
+parser.add_argument("pflicht_int", help="Integer der erforderlich ist", type=int)
+parser.add_argument("--optional_int", help="Optionaler Integer", type=int)
+parser.add_argument("--optional_str", help="Optionaler String")
+parser.add_argument("--wahl", help="Meine Wahl", choices=["a","b","c","d"])
+args = parser.parse_args()
+
+print("Integer:", args.pflicht_int, "- Typ:", type(args.pflicht_int))
+print("Optional Integer:", args.optional_int)
+print("Optional String:", args.optional_str, "- Type:", type(args.optional_str))
+print("Wahl:", args.wahl)
+print("ende")
