6a9b13ca20db0045edc2a0b238233bb0b9ec3374
boellerbernd <boellerbernd1000@gmail.com>
2025-05-26 10:45:02 +0000

    added

---
+++ beispiel_code.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+class Buch:
+    """Eine erweiterte Klasse zur Darstellung eines Buches im Bücherregal."""
+
+    def __init__(self, titel: str, autor: str):
+        """Initialisiert das Buch mit einem Titel und einem Autor."""
+        self.titel = titel
+        self.autor = autor
+        self._status = "verfügbar"
+    
+    def __str__(self) -> str:
+        """Gibt benutzerfreundliche Darstellung eines Buches zurück."""
+        return f"'{self.titel}' von {self.autor}"
+    
+    def __repr__(self) -> str:
+        """Gibt eine technische Darstellung des Buches zurück, nützlich für Debugging"""
+        return f"Buch(titel='{self.titel}, autor='{self.autor}', status='{self._status}')"
+    
+    def __call__(self) -> str:
+        """Macht die Instanz aufrugbar und gibt eine Statusmeldung zurück."""
+        return f"Das Buch '{self.titel}' ist aktuell {self._status}."
+    
+    def ausleihen(self):
+        """Markiert das Buch als ausgeliehen, wenn es verfügbar ist."""
+        if self._status == "verfügbar":
+            self._status = "ausgeliehen"
+            print(f"Das Buch '{self.titel}' wurde ausgeliehen.")
+        else:
+            print(f"Das Buch '{self.titel}' ist bereits ausgeliehen.")
+
+    def zurückgeben(self):
+        """Markiert das Buch als verfügbar."""
+        if self._status == "ausgeliehen":
+            self._status = "verfügbar"
+            print(f"Das Buch '{self.titel}' wurde zurückgegeben.")
+        else:
+            print(f"Das Buch '{self.titel}' ist bereits verfügbar.")
+
+    def get_status(self) -> str:
+        """Gibt den aktuellen Ausleihstatus des Buches zurück."""
+        return self._status
+
+# Erstellung und Nutzung der erweiterten Buch-Klasse
+buch = Buch("Der Herr der Ringe", "J.R.R. Tolkien")
+
+# __str__ wird aufgerufen, wenn ein Buch mit print ausgegeben wird
+print(buch)
+
+# __repr__ wird aufgerufen, wenn ein Buch im Debugging-Kontext ausgegeben wird
+print(repr(buch))
+
+# __call__ mach die Instanz aufrufbar wie eine Funktion
+print(buch()) #Ähnlich wie ein Funktionsaufruf
